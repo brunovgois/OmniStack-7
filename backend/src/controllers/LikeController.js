f23cae57013a40bb55f3b466adeefe8d2a7d2438
bruno.gois <brunovgois@gmail.com>
2019-06-15 14:27:38 -0300

    backend da aplicação finalizado

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -8,6 +8,7 @@ module.exports = {
 
         await post.save();
 
+        req.io.emit('like', post);
         return res.json(post);  
     }
 };
--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -13,15 +13,18 @@ module.exports = {
     async store(req, res) {
         const { author, place, description, hashtags} = req.body;
         const { filename: image } = req.file;
+        
+        const [name] = image.split('.');
+        const fileName = `${name}.jpg`;
 
         await sharp(req.file.path)
         .resize(500)
         .jpeg({ quality: 70 })
         .toFile(
-            path.resolve(req.file.destination, 'resized', image)
+            path.resolve(req.file.destination, 'resized', fileName)
         ) 
 
-        //deletar imagem
+        //deletar imagens nao redimensionadas
         fs.unlinkSync(req.file.path);
 
         const post = await Post.create({
@@ -29,9 +32,11 @@ module.exports = {
             place,
             description,
             hashtags,
-            image,
+            image: fileName,
         });
-         
+
+        req.io.emit('post', post);
+        
         return res.json(post);  
     }
 };
