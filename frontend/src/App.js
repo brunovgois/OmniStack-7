6e15f144869b59deccff770f6fad3ba89d33ceed
bruno.gois <brunovgois@gmail.com>
2019-06-16 17:42:30 -0300

    Setup inicial da parte de frontend, com as rotas feed e new setadas

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
